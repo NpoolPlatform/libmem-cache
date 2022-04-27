190b742f4484a169bc1ca622a312d3ebb6201215
Zhao KK <kikakkz@hotmailcom>
2022-04-27 08:44:25 +0000

    Add mixin in ent helper

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NpoolPlatform/libent-cruder
+
+go 1.17
+
+require entgo.io/ent v0.10.1
---
+++ pkg/mixin/time.go
@@ -0,0 +1,37 @@
+package mixin
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+type Schema struct {
+	mixin.Schema
+}
+
+type Policy interface {
+	Policy() ent.Policy
+}
+
+func (Schema) Fields() []ent.Field {
+	return []ent.Field{
+		field.Uint32("created_at").
+			DefaultFunc(func() uint32 {
+				return uint32(time.Now().Unix())
+			}),
+		field.Uint32("updated_at").
+			DefaultFunc(func() uint32 {
+				return uint32(time.Now().Unix())
+			}).
+			UpdateDefault(func() uint32 {
+				return uint32(time.Now().Unix())
+			}),
+		field.Uint32("deleted_at").
+			DefaultFunc(func() uint32 {
+				return 0
+			}),
+	}
+}
