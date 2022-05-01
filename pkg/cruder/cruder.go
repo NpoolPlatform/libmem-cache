cd616271fb5da595252092c646b9f4bbff4246f5
Zhao KK <kikakkz@hotmailcom>
2022-05-01 02:43:33 +0000

    Add cond helper

--- pkg/cruder/cruder.go
+++ pkg/cruder/cruder.go
@@ -22,18 +22,45 @@ type Cond struct {
 	Val Any
 }
 
+type Fields map[string]Any
+
+func NewFields() Fields {
+	return map[string]Any{}
+}
+
+func (f Fields) WithField(field string, val Any) Fields {
+	f[field] = val
+	return f
+}
+
+type Conds map[string]*Cond
+
+func NewConds() Conds {
+	return map[string]*Cond{}
+}
+
+func (c Conds) WithCond(field string, op string, val Any) Conds {
+	c[field] = &Cond{
+		Op:  op,
+		Val: val,
+	}
+	return c
+}
+
 type Cruder interface {
 	Create(ctx context.Context, in Any) (Any, error)
 	CreateBulk(ctx context.Context, in []Any) ([]Any, error)
 
 	Update(ctx context.Context, in Any) (Any, error)
 	UpdateFields(ctx context.Context, id uuid.UUID, fields map[string]Any) (Any, error)
-	AtomicUpdateFields(ctx context.Context, id uuid.UUID, fields map[string]Any) (Any, error)
+	AddFields(ctx context.Context, id uuid.UUID, fields map[string]Any) (Any, error)
+	SubFields(ctx context.Context, id uuid.UUID, fields map[string]Any) (Any, error)
 
 	Row(ctx context.Context, id uuid.UUID) (Any, error)
 	Rows(ctx context.Context, conds map[string]Cond, offset, limit uint32) ([]Any, error)
 
-	Exist(ctx context.Context, conds map[string]Cond) (bool, error)
+	Exist(ctx context.Context, id uuid.UUID) (bool, error)
+	ExistConds(ctx context.Context, conds map[string]Cond) (bool, error)
 	Count(ctx context.Context, conds map[string]Cond) (uint32, error)
 	// TODO: MAP | REDUCE | FILTER | SUM
 
